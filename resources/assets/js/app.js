5b49c991d4cbbb0d72139b7e0ad99f0636a3697f
logly-zhao <ab52525258@sina.com>
2018-09-11 06:09:03 +0000

    login and register

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -17,9 +17,6 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
-require('./components/SelectDistrict');
-require('./components/UserAddressesCreateAndEdit');
-
 const app = new Vue({
     el: '#app'
 });
