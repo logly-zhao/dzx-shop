b5bbd84071c25be659eb4a8eb0ca94f9e82e50f0
logly-zhao <ab52525258@sina.com>
2018-09-11 09:45:51 +0000

    manage user

---
+++ app/Admin/Controllers/UsersController.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\User;
+
+//use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Show;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Layout\Content;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\ModelForm;
+
+class UsersController extends Controller
+{
+    use ModelForm;
+//    use HasResourceActions;
+
+    /**
+     * Index interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function index(Content $content)
+    {
+        /*
+        return $content
+            ->header('Index')
+            ->description('description')
+            ->body($this->grid());
+        */
+        return Admin::content(function (Content $content) {
+            // 页面标题
+            $content->header('用户列表');
+            $content->body($this->grid());
+        });
+    }
+
+    /**
+     * Show interface.
+     *
+     * @param mixed   $id
+     * @param Content $content
+     * @return Content
+     */
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header('Detail')
+            ->description('description')
+            ->body($this->detail($id));
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param mixed   $id
+     * @param Content $content
+     * @return Content
+     */
+    public function edit($id, Content $content)
+    {
+        return $content
+            ->header('Edit')
+            ->description('description')
+            ->body($this->form()->edit($id));
+    }
+
+    /**
+     * Create interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+     /*
+    public function create(Content $content)
+    {
+        return $content
+            ->header('Create')
+            ->description('description')
+            ->body($this->form());
+    }
+*/
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new User);
+
+        $grid->id('ID')->sortable();
+        $grid->name('用户名');
+        $grid->email('邮箱');
+//      $grid->password('密码');
+//      $grid->remember_token('Remember token');
+        $grid->email_verified('已验证邮箱')->display(function ($value) {
+            return $value ? '是' : '否';
+        });
+        $grid->created_at('Created at');
+        $grid->disableCreateButton();
+        $grid->updated_at('Updated at');
+        $grid->actions(function ($actions) {
+            // 不在每一行后面展示查看按钮
+            $actions->disableView();
+
+            // 不在每一行后面展示删除按钮
+            $actions->disableDelete();
+
+            // 不在每一行后面展示编辑按钮
+            $actions->disableEdit();
+        });
+        $grid->tools(function ($tools) {
+
+            // 禁用批量删除按钮
+            $tools->batch(function ($batch) {
+                $batch->disableDelete();
+            });
+        });
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed   $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(User::findOrFail($id));
+
+        $show->id('Id');
+        $show->name('Name');
+        $show->email('Email');
+        $show->password('Password');
+        $show->remember_token('Remember token');
+        $show->email_verified('Email verified');
+        $show->created_at('Created at');
+        $show->updated_at('Updated at');
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new User);
+
+        $form->text('name', 'Name');
+        $form->email('email', 'Email');
+        $form->password('password', 'Password');
+        $form->text('remember_token', 'Remember token');
+        $form->switch('email_verified', 'Email verified');
+
+        return $form;
+    }
+}
